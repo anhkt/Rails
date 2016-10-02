1f390c33ef3dd0ec0e11c03dc6bcfba9ce226817
kieuthianh <anhkt1603@gmail.com>
2016-10-02 23:31:12 +0700

    Chap-5

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+  def about
+  end
+<<<<<<< HEAD
+
+  def contact
+  end
+
+  def signup
+  end
+=======
+>>>>>>> 34059f2... chap3
+end
