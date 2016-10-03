7ce5d132b9f9206ae519d74cd255293494a22547
kieuthianh <anhkt1603@gmail.com>
2016-10-03 15:30:17 +0700

    Chap8

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,3 +10,11 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
 
 end
+
+class << self
+  def digest string
+  	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST:
+  	                                              BCrypt::Engine.cost
+  	BCrypt::Password.create string, cost: cost
+  end
+end
