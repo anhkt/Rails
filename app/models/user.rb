b12d855dbe0f9e463647ccb8a0ffe6fad3244505
kieuthianh <anhkt1603@gmail.com>
2016-10-07 14:45:00 +0700

    Chap-9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  #attr_accsesor :remember_token
+  attr_accessor :remember_token
   before_save {self.email = email.downcase}
   validates :name,  presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -9,22 +9,29 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
 
-class << self
-  def digest string
-  	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST:
+  class << self
+    def digest string
+  	  cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST:
   	                                              BCrypt::Engine.cost
-  	BCrypt::Password.create string, cost: cost
-  end
-end
+  	  BCrypt::Password.create string, cost: cost
+    end
 
-class << self
-  def new_token
-    SecureRandom.urlsafe_base64
-  end
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
 
-  def remember
-    self.remember_token = ""
-    update_attribute :remember_digest
+    def remember
+      self.remember_token = User.new_token
+      update_attribute :remember_digest, User.digest(remember_token)
+    end
+
+    def authenticated?remember_token
+      return false if remember_digest.nil?
+      BCrypt::Password.new(remember_digest).is_password?remember_token
+    end
+
+    def forget
+      update_attribute :remember_digest, nil
+    end
   end
-end
 end
