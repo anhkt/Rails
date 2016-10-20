a1094828205ff03e55b409312a48c10d10852350
anhkt <anhkt1603@gmail.com>
2016-10-20 12:56:59 +0700

    chap14

--- config/routes.rb
+++ config/routes.rb
@@ -18,9 +18,15 @@ Rails.application.routes.draw do
 
   root "static_pages#home"
 
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   resources :users
   resources :account_activations, only: :edit
-  resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :password_resets, except: [:show, :destroy, :index]
   resources :microposts, only: [:create, :destroy]
 end
 
